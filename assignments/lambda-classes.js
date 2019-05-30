e59b316608ab23f485381f357708784d456b5628
Amber Sorensen <ambersorensen@Ambers-MacBook-Pro.local>
2019-05-29 20:55:50 -0700

    created lambda classes, without console.log, got back undefined with no errors, i think the code works smoothly, will test by console.log, will attempt stretch goals tomorrow

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,58 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attribute){
+        this.name=attribute.name;
+        this.age=attribute.age;
+        this.location=attribute.location;
+    }
+speak(){
+    return `Hello, my name is ${this.name}, I am from ${this.location}`;
+}
+}
+
+class Instructor extends Person{
+    constructor(attribute){
+        super(attribute);
+        this.specialty=attribute.specialty;
+        this.favLanguage=attribute.favLanguage;
+        this.catchPhrase=attribute.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`;
+    }
+    grade({student},subject){
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+class Student extends Person{
+    constructor(attribute){
+        super(attribute);
+        this.previousBackground=attribute.previousBackground;
+        this.className=attribute.className;
+        this.favSubjects=attribute.favSubjects;
+    }
+    listsSubjects(){
+        return `${this.favSubjects}`;
+    }
+    PRAssignment(subject){
+        `${student.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject){
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+class ProjectManagers extends Instructor{
+    constructor(attribute){
+        super(attribute);
+        this.gradClassName=attribute.gradClassName;
+        this.favInstructor=attribute.favInstructor;
+    }
+    standUp(name,channel){
+        return `${name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode({student},subject){
+        return `${name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
